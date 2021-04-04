5f27796dcd8fef235fceb4b3e8cc21b83de7028f
norb-gf <norb_gf@yahoo.com.br>
2021-04-04 19:09:17 -0300

    Projeto criado

---
+++ src/main/java/com/udemycursojava/aula305/Aula305Application.java
@@ -0,0 +1,13 @@
+package com.udemycursojava.aula305;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula305Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemycursojava/aula305/Aula305ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemycursojava.aula305;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula305ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
